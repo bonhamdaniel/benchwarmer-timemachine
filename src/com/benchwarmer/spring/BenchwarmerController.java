66f8fb7fc7753e1faa3b62615ba20916023da1b2
Daniel Bonham <bonhamdaniel@gmail.com>
2017-09-02 08:38:27 -0400

    Welcome update

--- src/com/benchwarmer/spring/BenchwarmerController.java
+++ src/com/benchwarmer/spring/BenchwarmerController.java
@@ -54,7 +54,7 @@ public class BenchwarmerController {
 		return players; // returns new player bio hashmap
 	} // searchableplayers()
 	
-	@RequestMapping({"/"}) // handles initial loading of the application
+	@RequestMapping({"/index"}) // handles initial loading of the application
 	public String index(@ModelAttribute("seasons") List<Season> seasons, @ModelAttribute("baseS") int baseS, @ModelAttribute("targetS") int targetS, @ModelAttribute("min") int min) {
 		return "index";
 	} // index()
